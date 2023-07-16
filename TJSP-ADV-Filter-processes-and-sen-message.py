f662e5cb35182715b2a7931da962f59b9584eb8f
luangrabher <luanggcontato@gmail.com>
2023-07-16 11:34:32 -0300

    criado todos do projeto

---
+++ TJSP-ADV-Filter-processes-and-sen-message.py
@@ -0,0 +1,18 @@
+# TODOS:
+# TODO: Conectar a arquivo de configuração json
+# TODO: Ler e-mails e achar ultimo e-mail nao lido com assunto '%Remessa do Dia%' e contém anexo .zip
+# TODO: Baixar arquivo zip do e-mail
+# TODO: Extrai arquivos do zip
+# TODO: Conforme a lista de filtros de arquivos .xlsx no arquivo de configuração extrai os dados de cada arquivo com os filtros necessários
+# TODO: Faz o filtro desses dados e pega o nro processo
+# TODO: Login TJSP
+# TODO: Entrar na pagina de detalhes do processo
+# TODO: Parar se for menor que R$ 100.000,00
+# TODO: Parar se no final ‘qto’ houver advogado
+# TODO: Pega detalhes do processo nome da pessoa/empresa, valor, datas
+# TODO: Pegar CNPJ/CPF em ‘Visualizar Autos’ → Petição Inicial
+# TODO: Login Assertiva
+# TODO: Consulta por CNPJ/CPF na Assertiva
+# TODO: Clica em ‘buscar relacionados’ até desaparecer e pega 3 primeiros numeros com whatsapp habilitado
+# TODO: Inserir Dados na planilha google
+# TODO: Chamar No Whatsapp. Criar link com o numero e mensagem personalizada abre o whatsapp web e envia a mensagem.
