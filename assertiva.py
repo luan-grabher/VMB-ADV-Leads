2c76ae4866a0a50491131820eef91f8266ed3296
luangrabher <luanggcontato@gmail.com>
2023-07-26 21:24:14 -0300

    feat - pega lista de telefones da lista de processos

--- assertiva.py
+++ assertiva.py
@@ -1,5 +1,7 @@
 
+import json
 import re
+import time
 import pandas as pd
 from config import getConfig
 from chromedriver_autoinstaller import install
@@ -7,7 +9,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-import requests
 
 regexCnpj = "\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}"
 regexCpf = "\d{3}\.\d{3}\.\d{3}-\d{2}"
@@ -22,23 +23,17 @@ def getProcessosComTelefone(config, processos):
 
     login(driver, configAssertiva)
 
-    processosComTelefone = pd.DataFrame()
+    processos['Telefone'] = None
 
     for index, processo in processos.iterrows():
         telefone = getTelefone(driver, configAssertiva, processo)
 
         if telefone:
-            processoComTelefone = processo.copy()
-            processoComTelefone['Telefone'] = telefone
+            processos.at[index, 'Telefone'] = telefone            
 
-            processosComTelefone = pd.concat([
-                processosComTelefone,
-                pd.DataFrame(processoComTelefone)
-            ])
-
-    input('Pressione enter para continuar...')
-    
     driver.quit()
+
+    return processos
 
 def login(driver, configAssertiva):
     loginConfig = configAssertiva['urls']['login']
@@ -57,6 +52,23 @@ def login(driver, configAssertiva):
     WebDriverWait(driver, 10).until(
         EC.url_matches(inicio_url)
     )
+
+def aceitarCookies(driver, configCookies):
+    isBotaoCookiesExists = True
+    while isBotaoCookiesExists:
+        try:
+            driver.find_element(By.CSS_SELECTOR, configCookies['botaoCookies']).click()
+            time.sleep(1)
+        except:
+            isBotaoCookiesExists = False
+    
+    isBotaoConfidencialAlertExists = True
+    while isBotaoConfidencialAlertExists:
+        try:
+            driver.find_element(By.CSS_SELECTOR, configCookies['btnConfidencialAlert']).click()
+            time.sleep(1)
+        except:
+            isBotaoConfidencialAlertExists = False
 
 
 def getTelefone(driver, configAssertiva, processo):
@@ -85,6 +97,8 @@ def getTelefone(driver, configAssertiva, processo):
         EC.presence_of_element_located((By.CSS_SELECTOR, consultaConfig['css']['tituloTelefones']))
     )
 
+    aceitarCookies(driver, configAssertiva['cookies'])
+
     divTelefones = None
     parent = tituloTelefones.find_element(By.XPATH, '..')
     while divTelefones is None:        
@@ -93,10 +107,42 @@ def getTelefone(driver, configAssertiva, processo):
             divTelefones = parent
         else:
             parent = parent.find_element(By.XPATH, '..')
+        
+    cardsTelefone = divTelefones.find_elements(By.CSS_SELECTOR, consultaConfig['css']['cardTelefone'])
+    cardsComWhatsapp = getCardTelefonesWithWhatsapp(driver, consultaConfig, cardsTelefone)
 
+    if len(cardsComWhatsapp) == 0:
+        isbtnMaisTelefonesExists = True
+        while isbtnMaisTelefonesExists:
+            try:
+                driver.find_element(By.CSS_SELECTOR, consultaConfig['css']['btnMaisTelefones']).click()
+                time.sleep(1)
+            except:
+                isbtnMaisTelefonesExists = False
 
-    input('Pressione enter para continuar...')
+        cardsTelefone = divTelefones.find_elements(By.CSS_SELECTOR, consultaConfig['css']['cardTelefone'])
+        cardsComWhatsapp = getCardTelefonesWithWhatsapp(driver, consultaConfig, cardsTelefone)
+
+    if len(cardsComWhatsapp) == 0:
+        return None
     
+    telefones = []
+    for card in cardsComWhatsapp:
+        telefone = card.find_element(By.CSS_SELECTOR, consultaConfig['css']['telefone']).text
+        telefones.append(telefone)
+    
+    return json.dumps(telefones)
+    
+def getCardTelefonesWithWhatsapp(driver, consultaConfig, cards):
+    cardsComWhatsapp = []
+    for card in cards:
+        botaoWhatsapp = card.find_element(By.CSS_SELECTOR, consultaConfig['css']['telefoneBotaoWhatsapp'])
+        hasDisableAttribute = botaoWhatsapp.get_attribute('disabled') is not None
+        if not hasDisableAttribute:
+            cardsComWhatsapp.append(card)
+        
+
+    return cardsComWhatsapp
     
 
 if __name__ == "__main__":
